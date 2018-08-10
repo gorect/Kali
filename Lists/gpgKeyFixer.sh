ba6a46441943fac2ae49fce69ff828f2e44de285
Cameron Gore <gorect@vcu.edu>
2018-08-10 08:42:36 -0400

    Update gpgKeyFixer.sh
    Added sleep function to see if that fixes run time error

--- Lists/gpgKeyFixer.sh
+++ Lists/gpgKeyFixer.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 gpg --keyserver hkp://keys.gnupg.net --recv-key 7D8D0BF6
+sleep 10s
 gpg --fingerprint 7D8D0BF6
+sleep 10s
 gpg -a --export 7D8D0BF6 | apt-key add -
+sleep 10s
 apt update
