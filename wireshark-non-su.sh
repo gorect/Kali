7562e295d408943efbe3729ed508175a72d2df7b
Cameron Gore <gorect@vcu.edu>
2019-02-04 10:13:44 -0500

    Delete wireshark-non-su.sh

--- wireshark-non-su.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-## This script is for fixing the error that does not allow non root users to be able to capture packets on Wireshark
-sudo dpkg-reconfigure wireshark-common
-sudo gpasswd -a $USER wireshark
