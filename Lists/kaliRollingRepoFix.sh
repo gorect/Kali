1ddc519d0916ac860f1c0f825cafa61566bb3178
Cameron Gore <gorect@vcu.edu>
2019-08-12 10:52:10 -0400

    Update kaliRollingRepoFix.sh

--- Lists/kaliRollingRepoFix.sh
+++ Lists/kaliRollingRepoFix.sh
@@ -2,3 +2,5 @@
 sed "/deb https://http.kali.org/kali kali-rolling main non-free contrib/d" </etc/apt/sources.list> /etc/apt/sources.fixed
 mv -f /etc/apt/sources.fixed /etc/apt/sources.list
 echo "deb https://http.kali.org/kali kali-rolling main contrib non-free" >> /etc/apt/sources.list
+echo "apt-get update -y"
+apt-get update -y
