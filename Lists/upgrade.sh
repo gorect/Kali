c83e1ebb846e60afb4bda8823ab3cf8e4bd21a8c
Cameron Gore <gorect@vcu.edu>
2019-08-12 12:29:18 -0400

    Update upgrade.sh

--- Lists/upgrade.sh
+++ Lists/upgrade.sh
@@ -3,7 +3,13 @@ echo "sudo apt-get update"
 sudo apt-get update
 echo "sudo apt-get upgrade"
 sudo apt-get upgrade -y
+echo "apt-get dist-upgrade"
+sudo apt-get dist-upgrade -y
+echo "apt-get full-upgrade"
+sudo apt-get full-upgrade -y
 echo "sudo apt-get install"
 sudo apt-get install -y
 echo "sudo apt-get autoremove"
 sudo apt-get autoremove -y
+echo "apt-get clean"
+sudo apt-get clean -y
