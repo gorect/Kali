e66491f1e734979476499b518aadfb0318833752
Cameron Gore <gorect@vcu.edu>
2019-08-12 10:50:03 -0400

    Update kaliRollingRepoFix.sh
    This fix reflects the current need for the repo to look for the secure dns entry (https). Otherwise the update/upgrade process may fail.

--- Lists/kaliRollingRepoFix.sh
+++ Lists/kaliRollingRepoFix.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-sed "/deb http://http.kali.org/kali kali-rolling main non-free contrib/d" </etc/apt/sources.list> /etc/apt/sources.fixed
+sed "/deb https://http.kali.org/kali kali-rolling main non-free contrib/d" </etc/apt/sources.list> /etc/apt/sources.fixed
 mv -f /etc/apt/sources.fixed /etc/apt/sources.list
-echo "deb http://http.kali.org/kali kali-rolling main contrib non-free" >> /etc/apt/sources.list
-
+echo "deb https://http.kali.org/kali kali-rolling main contrib non-free" >> /etc/apt/sources.list
