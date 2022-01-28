47b87c9b6bc90c36103145870bd4d32f0a2d2739
gorect <cameron.t.gore@gmail.com>
2022-01-28 00:33:11 +0000

    More clean up of old files.

--- First-Time-Setup-Script/Lists/gpgKeyFixer.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-gpg --keyserver hkp://keys.gnupg.net --recv-key 7D8D0BF6
-sleep 10s
-gpg --fingerprint 7D8D0BF6
-sleep 10s
-gpg -a --export 7D8D0BF6 | apt-key add -
-sleep 10s
-apt update
